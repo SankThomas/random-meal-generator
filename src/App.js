264d1672b93d0de3c0edcef53b2e4cd94bd899e5
Thomas Sankara <thomassankara74@gmail.com>
2021-01-24 20:48:15 +0300

    add ingredients and meal tags

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (
     <>
       <div>
-        <h1>Random meal generator</h1>
-        <div className='underline'></div>
+        <h1 className="font-bold text-4xl lg:text-6xl mt-10">
+          Random meal generator
+        </h1>
+        <div className="underline mx-auto mt-4 mb-5"></div>
         <Meal />
       </div>
     </>
